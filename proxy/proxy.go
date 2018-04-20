62b2f5678e5e731fb583cfeaa309f0c0b821df5c
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2018-04-20 11:18:30 +0200

    Add Andreas example; periodically ping the service manager;
     - Add Andreas example to to a new file 'servicecatalog', which provides
     methods to list/register/unregister service brokers form the k8s
     serivce catalog.
     - Add function to periodically ping the service manager.
     - Formatting.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,10 +1,10 @@
 package proxy
 
 import (
+	"log"
 	"net/http"
 	"strings"
 	"time"
-	"log"
 )
 
 // Proxy is the service broker proxy. The Proxy listens to all incoming requests and proxies them to the service manager.
@@ -14,8 +14,9 @@ type Proxy struct {
 	username string
 	password string
 }
-// New creates a new instance of Proxy with the configuration from the environment variables stored in EnvConfig.
-func New() Proxy {
+
+// NewProxy creates a new instance of Proxy with the configuration from the environment variables stored in EnvConfig.
+func NewProxy() Proxy {
 	env := EnvConfig()
 	return Proxy{
 		&env,
@@ -27,6 +28,10 @@ func New() Proxy {
 
 // Start starts the service broker proxy on port 8080. This function runs as long as the server is up and running.
 func (proxy Proxy) Start() error {
+	// Periodically check for service brokers
+	go pingServiceManager(5)
+
+	// Start the proxy
 	http.HandleFunc("/", proxy.listen)
 	return http.ListenAndServe(":8080", nil)
 }
@@ -64,12 +69,12 @@ func (proxy Proxy) listen(writer http.ResponseWriter, request *http.Request) {
 	brokerURLFragment := proxy.getBrokerIDFromURLFragment(request.URL.Path)
 	log.Println("[proxy.go; listen()] broker url after getBrokerIdFromURLFragment: " + brokerURLFragment)
 
-	brokerURL := proxy.url + "/v1/osb/" + brokerURLFragment;
-	log.Println("[proxy.go; listen()] broker url: " + brokerURL);
+	brokerURL := proxy.url + "/v1/osb/" + brokerURLFragment
+	log.Println("[proxy.go; listen()] broker url: " + brokerURL)
 
 	//TODO error handling
 	client := &http.Client{
-		Timeout: time.Duration(proxy.config.timeoutSeconds) * time.Second,
+		Timeout:   time.Duration(proxy.config.timeoutSeconds) * time.Second,
 		Transport: &loggingRoundTripper{},
 	}
 	smRequest, _ := http.NewRequest(request.Method, brokerURL, request.Body)
